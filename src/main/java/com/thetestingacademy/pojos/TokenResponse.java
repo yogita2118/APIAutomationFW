2d5eca12150c82e0af62306cc7f9bf90bd1a10ca
Yogita Solanki <yogita2118@gmail.com>
2024-07-05 15:26:45 -0400

    Added a basic Structure

---
+++ src/main/java/com/thetestingacademy/pojos/TokenResponse.java
@@ -0,0 +1,16 @@
+package com.thetestingacademy.pojos;
+
+public class   TokenResponse {
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+
+}
